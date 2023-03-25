4582802029f0b426dca061607a10364d77bd0a83
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-25 17:39:00 +0600

    Fetching data with react query

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import router from './Routes/Routes'
 function App() {
  
   return (
-    <div>
+    <>
       <RouterProvider router={router}></RouterProvider>
-    </div>
+    </>
     
   )
 }
--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import React from 'react';
+import { useQuery } from 'react-query';
 
+const fetchSuperHeroes = () =>{
+    return axios.get('http://localhost:4000/superheroes')
+}
 const RQSuperHeroesPage = () => {
+    const {isLoading,data} = useQuery('super-heroes',fetchSuperHeroes)
+    if(isLoading){
+        return <h1>Loading...</h1>
+    }
     return (
         <div>
             <h2>RQSuper Hero</h2>
+            {
+                data?.data.map(hero =>{
+                    return <div key={hero.name}>{hero.name}</div>
+                })
+            }
         </div>
     );
 };
