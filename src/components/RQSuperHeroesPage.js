e0debbd689a4e43b9f7179242ed247da4552bfcd
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-25 19:33:02 +0600

    Error handling in traditional and react query way

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -6,9 +6,12 @@ const fetchSuperHeroes = () =>{
     return axios.get('http://localhost:4000/superheroes')
 }
 const RQSuperHeroesPage = () => {
-    const {isLoading,data} = useQuery('super-heroes',fetchSuperHeroes)
+    const {isLoading,data,isError,error} = useQuery('super-heroes',fetchSuperHeroes)
     if(isLoading){
         return <h1>Loading...</h1>
+    }
+    if(isError){
+        return <h1>{error.message}</h1>
     }
     return (
         <div>
