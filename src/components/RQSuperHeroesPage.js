d3900d83eab3a618cdbeba6b71408f7989328ceb
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-26 21:29:20 +0600

    callbacks for side effects are used

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
 import React from 'react';
+import { toast } from 'react-hot-toast';
 import { useQuery } from 'react-query';
 
 const fetchSuperHeroes = () =>{
     return axios.get('http://localhost:4000/superheroes')
 }
 const RQSuperHeroesPage = () => {
+    const onSuccess = (data) =>{ //react query can pass the data or the error that is encountered while fetching in this callbacks. So we can pass that in the form of a parameter in the function.
+        toast.success('Perform side effect after data fetching');
+        console.log(data);
+    }
+    const onError = (error) =>{
+        toast.error('Perform side effect after data fetching');
+        console.log(error);
+    }
     const {isLoading,
         data,
         isError,
@@ -23,7 +32,9 @@ const RQSuperHeroesPage = () => {
             // refetchInterval: false, default value  
             // refetchInterval: 2000, //data will automatically being fetched after every 2 seconds
             // refetchIntervalInBackground: true, // data will be fetched in the background even when the browser is not in progress. 
-            enabled: false, //data fetching is disabled by this and it will be enabled with user event.
+            // enabled: false, //data fetching is disabled by this and it will be enabled with user event.
+            onSuccess: onSuccess, //callbacks used as a side effect
+            onError: onError, //callbacks used as a side effects
         })
 
     if(isLoading || isFetching){
