72bdfde57c9693645aca34473748e1f09cda0d84
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-26 21:12:19 +0600

    adding refetching method

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -10,6 +10,7 @@ const RQSuperHeroesPage = () => {
         data,
         isError,
         error,
+        refetch,
         isFetching} = useQuery('super-heroes',fetchSuperHeroes,{
             // cacheTime: 5000,
             // staleTime: 30000,
@@ -20,11 +21,12 @@ const RQSuperHeroesPage = () => {
             // refetchOnWindowFocus: true, //always updates data whenever it is changed. 
             //it can also be set as 'always' and false.
             // refetchInterval: false, default value  
-            refetchInterval: 2000, //data will automatically being fetched after every 2 seconds
-            refetchIntervalInBackground: true, // data will be fetched in the background even when the browser is not in progress. 
+            // refetchInterval: 2000, //data will automatically being fetched after every 2 seconds
+            // refetchIntervalInBackground: true, // data will be fetched in the background even when the browser is not in progress. 
+            enabled: false, //data fetching is disabled by this and it will be enabled with user event.
         })
 
-    if(isLoading){
+    if(isLoading || isFetching){
         return <h1>Loading...</h1>
     }
     console.log(isLoading,isFetching);
@@ -34,6 +36,7 @@ const RQSuperHeroesPage = () => {
     return (
         <div>
             <h2>RQSuper Hero</h2>
+            <button onClick={refetch}>Fetch Heroes</button>
             {
                 data?.data.map(hero =>{
                     return <div key={hero.name}>{hero.name}</div>
