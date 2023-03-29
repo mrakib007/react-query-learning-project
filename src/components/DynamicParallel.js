901a3ce1f9a8696792bfccd469f4cf05df0e8e24
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-29 17:53:29 +0600

    Adding dynamic parallel query result

--- src/components/DynamicParallel.js
+++ src/components/DynamicParallel.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import axios from 'axios';
-import { useQueries } from 'react-query';
+import React from "react";
+import axios from "axios";
+import { useQueries } from "react-query";
 
 const fetchSuperHero = (heroId) => {
-    return axios.get(`http://localhost:4000/superheroes/${heroId}`)
-}
-const DynamicParallel = ({heroIds}) => {
-    //useQueries returns an array of query results
-    const queryResults = useQueries(
-        heroIds.map(id => {
-            return {
-                queryKey: ['super-hero',id],
-                queryFn: () => fetchSuperHero(id), 
-            }
-        })
-    )
-    console.log({queryResults});
-    return (
-        <div>
-            This is dynamic parallel with useQueries.
-          <div> 
-            </div> 
-        </div>
-    );
+  return axios.get(`http://localhost:4000/superheroes/${heroId}`);
+};
+const DynamicParallel = ({ heroIds }) => {
+  //useQueries returns an array of query results
+  const queryResults = useQueries(
+    heroIds.map((id) => {
+      return {
+        queryKey: ["super-hero", id],
+        queryFn: () => fetchSuperHero(id),
+      };
+    })
+  );
+  return (
+    <div>
+      This is dynamic parallel with useQueries.
+      <div>
+        {/* {
+            queryResults?.map((result) => console.log(result?.data?.data?.name))
+        } */}
+
+        {
+            queryResults?.map((result)=>{
+                return <div key={result?.data?.data?.id}>{result?.data?.data?.name}</div>
+            })
+        }
+      </div>
+    </div>
+  );
 };
 
 export default DynamicParallel;
