72cf2142429c38a4351123aceff9b8b1d5255d93
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-30 00:33:42 +0600

    initialData added

--- src/hoooks/useSuperHeroData.js
+++ src/hoooks/useSuperHeroData.js
@@ -1,10 +1,23 @@
 import axios from "axios"
-import { useQuery } from "react-query"
+import { useQuery,useQueryClient } from "react-query"
 
 const fetchSuperHero = ({queryKey}) =>{
     const heroId = queryKey[1];
     return axios.get(`http://localhost:4000/superheroes/${heroId}`)
 }
 export const useSuperHeroData = (heroId) =>{
-    return useQuery(['super-hero',heroId],fetchSuperHero)
+    const queryClient = useQueryClient()
+    return useQuery(['super-hero',heroId],fetchSuperHero,{
+        initialData: () =>{ //this initial data helps to remove loading state with every click on the superhero listing page.
+            const hero = queryClient.getQueryData('super-heroes')?.data?.find(hero =>
+                 hero.id === parseInt(heroId))
+            if(hero){
+                return{
+                    data: hero
+                }
+            }else{
+                return undefined; //if this happens then react query will set this query to a hard loading state to prevent runtime error
+            }
+        }
+    })
 }
