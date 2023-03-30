1c8742d8f2e033e074bafdf161716c6107358463
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-30 23:01:53 +0600

    setQueryData added for more optimization

--- src/hoooks/useSuperHeroesData.js
+++ src/hoooks/useSuperHeroesData.js
@@ -24,11 +24,26 @@ export const useAddSuperHeroData = () =>{
     //for posting data to the json database
     const queryClient = useQueryClient();
     return useMutation(addSuperHero,{
-        onSuccess: () =>{
+        onSuccess: (data) =>{
+            //here data refers to the entire response to the post request
+            queryClient.setQueryData('super-heroes',(oldQueryData)=>{
+                //oldQueryData refers what is present in the query cache
+                return {
+                    ...oldQueryData,
+                    data: [...oldQueryData.data,data.data] //data.data refers the hero object of the mutation response
+                }
+            }) //this function is used for updating the query cache
+
+            //this is for invalidateQueries
             //with this, without any fetching button click, the
             //added data will be automatically fetched and shown in the page
             //here background refetch is initiated
-            queryClient.invalidateQueries('super-heroes')
+
+            // queryClient.invalidateQueries('super-heroes')
+
+            //with this invalidateQueries there will be an additional network request
+            //but it returns an object containing the same data we are getting by that request.
+            //so there is a room for improvement
         },
     });
 }
