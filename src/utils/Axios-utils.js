c9db8609fda859f65780b3e67d7523bb6d897e6f
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-31 11:48:41 +0600

    Added a comment

--- src/utils/Axios-utils.js
+++ src/utils/Axios-utils.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //axios interceptor
 const client = axios.create({baseURL: 'http://localhost:4000'})
-export const request = ({...options}) =>{
+export const request = ({...options}) =>{//options here are all the options that axios accepts
     client.defaults.headers.common.Authorization = `Bearer token`
     const onSuccess = response => response;
     const onError = error =>{
