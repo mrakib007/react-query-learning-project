53e9a6a321044ed01e235739d2f58d16b5d7a074
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-31 11:46:45 +0600

    axios interceptor

--- src/hoooks/useSuperHeroesData.js
+++ src/hoooks/useSuperHeroesData.js
@@ -1,12 +1,17 @@
-import axios from "axios";
+// import axios from "axios";
 import { useMutation, useQuery, useQueryClient } from "react-query";
+import { request } from "../utils/Axios-utils";
 
 const fetchSuperHeroes = () =>{
-    return axios.get('http://localhost:4000/superheroes')
+    // return axios.get('http://localhost:4000/superheroes')
+    //axios interceptor
+    return request({url: '/superheroes'})
 }
 
 const addSuperHero = (hero) =>{
-    return axios.post('http://localhost:4000/superheroes',hero)
+    // return axios.post('http://localhost:4000/superheroes',hero)
+    //axios interceptor
+    return request({url: '/superheroes',method: 'post',data: hero})
 }
 
 export const useSuperHeroesData = (onSuccess,onError) => {
